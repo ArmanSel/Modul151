152acafd59d97ab43fe04a6de7ef35cfc7e07cf6
FreshGingerGabriel <gabrielvarga82@gmail.com>
2023-01-16 16:28:44 +0100

    Created stored procedures for get, delete, post and put

--- SQLSchema/M151.sql
+++ SQLSchema/M151.sql
@@ -26,7 +26,7 @@ CREATE TABLE tw_Teams (
 INSERT INTO `m151`.`tw_teams` (`TeamName`, `TeamLeague`) VALUES ('Manchester United', 'Premier League');
 INSERT INTO `m151`.`tw_teams` (`TeamName`, `TeamLeague`) VALUES ('Burnley', 'Premier League');
 INSERT INTO `m151`.`tw_teams` (`TeamName`, `TeamLeague`) VALUES ('Newcastle United', 'Premier League');
-INSERT INTO `m151`.`tw_teams` (`TeamName`, `TeamLeague`) VALUES ('Bayern München', 'Bundesliga');
+INSERT INTO `m151`.`tw_teams` (`TeamName`, `TeamLeague`) VALUES ('Bayern Muenchen', 'Bundesliga');
 INSERT INTO `m151`.`tw_teams` (`TeamName`, `TeamLeague`) VALUES ('Juventus Turin', 'Serie A');
 INSERT INTO `m151`.`tw_teams` (`TeamName`, `TeamLeague`) VALUES ('Benfica', 'Liga Portugal');
 INSERT INTO `m151`.`tw_teams` (`TeamName`, `TeamLeague`) VALUES ('Liverpool', 'Premier League');
@@ -50,3 +50,65 @@ USE `m151`;
 CREATE  OR REPLACE VIEW `tw_v_transferOverview` AS
 SELECT tr.TransferId, p.PlayerFirstName AS 'First Name', p.PlayerLastName AS 'Last Name', p.PlayerAge AS 'Age', PlayerNationality AS 'Nationality', p.PlayerPosition AS 'Position', (SELECT TeamName FROM tw_teams WHERE TeamId = OldTeamId) AS 'Old Team', (SELECT TeamName FROM tw_teams WHERE TeamId  = NewTeamId) AS 'New Team', (SELECT IF(tr.TransferSum = 0, 'Free', tr.TransferSum)) AS 'Transfer Fee'
 FROM tw_transfers tr LEFT JOIN tw_players p ON tr.PlayerId = p.PlayerId;
+
+USE `m151`;
+DROP procedure IF EXISTS `tw_getTransfers`;
+
+DELIMITER $$
+USE `m151`$$
+CREATE PROCEDURE `tw_getTransfers` (IN input VARCHAR(255))
+BEGIN
+	SELECT * FROM tw_v_transferOverview WHERE TransferId = input;
+END$$
+
+DELIMITER ;
+
+USE `m151`;
+DROP procedure IF EXISTS `tw_deleteTransfer`;
+
+DELIMITER $$
+USE `m151`$$
+CREATE PROCEDURE `tw_deleteTransfer` (IN input VARCHAR(255))
+BEGIN
+	DELETE FROM tw_transfers WHERE TransferId = input;
+END$$
+
+DELIMITER ;
+
+USE `m151`;
+DROP procedure IF EXISTS `tw_updateTransfer`;
+
+DELIMITER $$
+USE `m151`$$
+CREATE PROCEDURE `tw_updateTransfer` (IN input INT, IN playerId INT, IN oldTeamId INT, IN newTeamId INT, IN transferSum INT)
+BEGIN
+	UPDATE tw_transfers SET PlayerId = playerId, OldTeamId = oldTeamId, NewTeamId = newTeamId, TransferSum = transferSum WHERE TransferId = input;
+END$$
+
+DELIMITER ;
+
+USE `m151`;
+DROP procedure IF EXISTS `tw_insertTransfer`;
+
+DELIMITER $$
+USE `m151`$$
+CREATE PROCEDURE `tw_insertTransfer` (IN playerId INT, IN oldTeamId INT, IN newTeamId INT, IN transferSum INT)
+BEGIN
+	INSERT INTO tw_transfers (PlayerId, OldTeamId, NewTeamId, TransferSum) VALUES (playerId, oldTeamId, newTeamId, transferSum);
+END$$
+
+DELIMITER ;
+
+USE `m151`;
+DROP procedure IF EXISTS `tw_getTransfersData`;
+
+DELIMITER $$
+USE `m151`$$
+CREATE PROCEDURE `tw_getTransfersData` (IN input INT)
+BEGIN
+	SELECT * FROM tw_transfers WHERE TransferId = input;
+END$$
+
+DELIMITER ;
+
+UPDATE tw_transfers SET PlayerId = 2, OldTeamId = 1, NewTeamId = 2, TransferSum = 1000 WHERE TransferId = 6;
